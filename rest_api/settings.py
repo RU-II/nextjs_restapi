785dc6d16c02d909e6a29e2a824faed955622533
Rui Kumagai <kmri.nnnnn@gmail.com>
2022-01-12 20:30:48 +0900

    add host url

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nextjs-blog-todo-api-ruii.herokuapp.com']
 
 
 # Application definition
